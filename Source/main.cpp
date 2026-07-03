84cb1109a19c20097192a370becc56ba53fe323a
James Byrne <jamespatbyrne60@gmail.com>
2026-07-03 20:48:58 +0800

    Experiment with UI palette

--- Source/main.cpp
+++ Source/main.cpp
@@ -10,6 +10,13 @@ int main(int argc, char *argv[])
     app.setApplicationDisplayName(globals::appName);
     dbManager::initialise();
 
+    QApplication::setStyle("Fusion");
+
+    QPalette pal = qApp->palette();
+    pal.setColor(QPalette::Highlight, QColor(255, 128, 0));
+    pal.setColor(QPalette::Accent, QColor(255, 128, 0));
+    qApp->setPalette(pal);
+
     MainWindow w;
 \
     w.show();
